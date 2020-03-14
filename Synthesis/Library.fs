f65192995a2d30982559a2713268a8b5fb12c8e7
g17e4476 <g17e4476@campus.ru.ac.za>
2020-03-14 21:31:44 +0200

    2 tests need solving

--- Synthesis/Library.fs
+++ Synthesis/Library.fs
@@ -1,5 +1,8 @@
 ﻿module Synthesis
 
+open System
+open System
+
 let abelar x = (x > 12 && x < 3097 && x%12=0 )
   //  failwith "Not implemented"
 
@@ -12,41 +15,142 @@ let area b h =
    
     // failwith "Not implemented"
 
-let zollo _ =
-    failwith "Not implemented"
+let zollo a =
+   match a > 0 with
+   | true -> a*2
+   | false -> a*(-1)
+    //failwith "Not implemented"
 
-let min _ _ =
-    failwith "Not implemented"
+let min a b =
+   match a > b with
+   |true -> b
+   |false -> a
+//    failwith "Not implemented"
 
-let max _ _ =
-    failwith "Not implemented"
+let max a b =
+    match a < b with
+   |true -> b
+   |false -> a
 
-let ofTime _ _ _ =
-    failwith "Not implemented"
+let ofTime h m s = h*60*60+m*60+s
+    // failwith "Not implemented"
 
-let toTime _ =
-    failwith "Not implemented"
+let toTime s = 
+   match s > 0 with
+   | true -> ((s/3600),((s-((s/3600)*3600))/60),(s-(s-(s-(((s-((s/3600)*3600))/60))*60)%60))) 
+   | false -> (0,0,0)
+   // failwith "Not implemented"
 
-let digits _ =
-    failwith "Not implemented"
+let digits n =
+   let rec count n digitnr = 
+      match n<>0 with
+      | true -> count (n/10) (digitnr+1)
+      | false -> max digitnr 1     
+   count n 0 
+    //failwith "Not implemented"
 
-let minmax _ =
-    failwith "Not implemented"
+let minmax (a,b,c,d) = (min (min a b)(min c d) , max(max a b)(max c d))
 
-let isLeap _ =
-    failwith "Not implemented"
 
-let month _ =
-    failwith "Not implemented"
 
-let toBinary _ =
-    failwith "Not implemented"
+    //failwith "Not implemented"
 
-let bizFuzz _ =
-    failwith "Not implemented"
+let isLeap y =
+   match y < 1582 with
+   | true -> failwith "Can't be less than year leap was implemented"
+   | false -> match ((y % 4 = 0) && ((y % 100 <> 0) || (y % 400 = 0) )) with
+              | true -> true
+              | false -> false
+   
+   
+  
+let month m =
+   match m with
+   | 1 -> ("January",31)
+   | 2 -> ("February",28)
+   | 3 -> ("March",31)
+   | 4 -> ("April",30)
+   | 5 -> ("May",31)
+   | 6 -> ("June",30)
+   | 7 -> ("July",31)
+   | 8 -> ("August",31)
+   | 9 -> ("September",30)
+   | 10 -> ("October",31)
+   | 11 -> ("November",30)
+   | 12 -> ("December",31)
+   | _ -> failwith "Not a valid int"
 
-let monthDay _ _ =
-    failwith "Not implemented"
+let toBinary i =
+   match i < 0 with
+   |true -> failwith "NOPE"
+   |false -> match i=0 with
+             | true -> "0"
+             | false -> let rec binary i =
+                           match (i=0) with
+                           | true -> ""
+                           | false -> match i%2=0 with
+                                      |true -> binary(i/2)+"0"
+                                      |false ->binary(i/2)+"1"
+                        binary i 
 
+
+let bizFuzz n =
+   let rec fizzing (m,c3s,c5s,both) = 
+      match (m <0 || m=1) with
+      | true -> (c3s,c5s,both)
+      | false -> match m%3=0 with
+                 | true -> match m%5=0 with
+                           | true -> fizzing (m-1,c3s+1,c5s+1,both+1)
+                           | false -> fizzing(m-1,c3s+1,c5s,both)
+                 | false -> match m%5=0 with
+                            | true -> fizzing(m-1,c3s,c5s+1,both)
+                            | false -> fizzing(m-1,c3s,c5s,both)
+   
+   fizzing (n,0,0,0) 
+   // failwith "Not implemented"
+
+let monthDay day y =
+//   match (y < 1582) with
+//   |true -> failwith "too soon bro"
+//   |false -> match (isLeap y) with
+//             | true -> match (d < 1 || d > 366) with
+//                       | true -> failwith "No day"
+//                       | false -> match d > 31 && d < 61 with
+//                                  |true -> "February"
+//                                  |false -> 
+//                                    let m,_ = month (max (d/30) 1) 
+//                                    m
+//             | false -> match (d < 1 || d > 365) with
+//                        | true -> failwith "No day"
+//                        | false -> match d > 31 && d < 60 with
+//                                   |true -> "February"
+//                                   |false -> 
+//                                     let i = fun (m,_ = month (max (d/30) 1) 
+//                                     m 
+//let monthDay day year =
+   match y < 1582 with
+   | true -> "Too Soon"
+   | false -> match (isLeap y) with
+              | true ->  match (day < 1 || day > 366) with
+                         | true -> failwith "Not a day"
+                         | false -> let rec MonthCalc mn dn =
+                                     let leap = [31;28;31;30;31;30;31;31;30;31;30;31]
+                                     match dn > 0 with
+                                     |true  -> MonthCalc (mn+1) dn-leap[mn] 
+                                     |false -> let m,_ = month(mn-1)
+                                               m
+                                    MonthCalc 1 day
+              |false -> match (day < 1 || day > 365) with
+                        | true -> failwith "Not a day"
+                        | false -> let rec MonthCalc mn dn =
+                                     let reg = [31,28,31,30,31,30,31,31,30,31,30,31]
+                                     match dn > 0 with
+                                     |true  -> MonthCalc (mn+1) dn-reg[mn] 
+                                     |false -> let m,_ = month(mn-1)
+                                               m
+                                   MonthCalc 1 day
+  
+  //
+  //[31;28;31;30;31;30;31;31;30;31;30;31]
 let coord _ =
     failwith "Not implemented"
